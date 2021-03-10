0f59e7d34bbbbe68c9be10d2aa573e3243242ba6
Amanda Meneghin <amanda.meneghin@hadrion.com.br>
2021-03-10 11:57:29 -0300

    Adicionando arquivo bin

--- buscador-cursos.php
+++ buscador-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -6,12 +7,12 @@ use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$client = new Client(['base_uri' => 'https://www.alura.com.br']);
 $crawler = new Crawler();
 
 $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo $curso . PHP_EOL;
+    exibeMensagem($curso);
 }
